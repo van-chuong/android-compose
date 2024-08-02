6361f543ba96370a2c3fc246161fdeae7b4f6205
Van Chuong <134tutu431@gmail.com>
2024-08-02 09:30:24 +0700

    learn theming

--- ThemingCodelab/app/src/main/java/com/example/reply/ui/MainActivity.kt
+++ ThemingCodelab/app/src/main/java/com/example/reply/ui/MainActivity.kt
@@ -22,11 +22,14 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
+import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.reply.data.LocalEmailsDataProvider
+import com.example.reply.ui.theme.ReplyTheme
 
 
 class MainActivity : ComponentActivity() {
@@ -38,15 +41,17 @@ class MainActivity : ComponentActivity() {
 
         setContent {
             val uiState by viewModel.uiState.collectAsState()
-            ReplyApp(
-                replyHomeUIState = uiState,
-                closeDetailScreen = {
-                    viewModel.closeDetailScreen()
-                },
-                navigateToDetail = { emailId ->
-                    viewModel.setSelectedEmail(emailId)
-                }
-            )
+            ReplyTheme {
+                ReplyApp(
+                    replyHomeUIState = uiState,
+                    closeDetailScreen = {
+                        viewModel.closeDetailScreen()
+                    },
+                    navigateToDetail = { emailId ->
+                        viewModel.setSelectedEmail(emailId)
+                    }
+                )
+            }
         }
     }
 }
@@ -61,9 +66,13 @@ class MainActivity : ComponentActivity() {
 )
 @Composable
 fun ReplyAppPreviewLight() {
-    ReplyApp(
-        replyHomeUIState = ReplyHomeUIState(
-            emails = LocalEmailsDataProvider.allEmails
-        )
-    )
+    ReplyTheme {
+        Surface(tonalElevation = 5.dp) {
+            ReplyApp(
+                replyHomeUIState = ReplyHomeUIState(
+                    emails = LocalEmailsDataProvider.allEmails
+                )
+            )
+        }
+    }
 }
--- ThemingCodelab/app/src/main/java/com/example/reply/ui/components/ReplyEmailThreadItem.kt
+++ ThemingCodelab/app/src/main/java/com/example/reply/ui/components/ReplyEmailThreadItem.kt
@@ -16,6 +16,7 @@
 
 package com.example.reply.ui.components
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -48,6 +49,10 @@ fun ReplyEmailThreadItem(
         modifier = modifier
             .fillMaxWidth()
             .padding(16.dp)
+            .background(
+                MaterialTheme.colorScheme.background,
+                MaterialTheme.shapes.medium
+            )
             .padding(20.dp)
     ) {
         Row(modifier = Modifier.fillMaxWidth()) {
